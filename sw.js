31df48be814e175e2254b70f4a97a0f72f520028
SNAMY tech <hello.snamy@gmail.com>
2025-09-30 18:54:14 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'telugu-news-v1';
+const urlsToCache = [
+    '/',
+    '/css/style.css',
+    '/css/marketing.css',
+    '/js/marketing.js',
+    '/images/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+self.addEventListener('push', (event) => {
+    const options = {
+        body: event.data.text(),
+        icon: '/images/icon-192.png',
+        badge: '/images/badge-72.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore',
+                title: 'Read More',
+                icon: '/images/icon-72.png'
+            },
+            {
+                action: 'close',
+                title: 'Close',
+                icon: '/images/icon-72.png'
+            },
+        ]
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('తెలుగు వార్తలు', options)
+    );
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    
+    if (event.action === 'explore') {
+        event.waitUntil(
+            clients.openWindow('/latest-news')
+        );
+    }
+});
