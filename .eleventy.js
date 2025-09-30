208563b358809a4a3f0b81b6741db361fd65bf41
SNAMY tech <hello.snamy@gmail.com>
2025-09-30 19:25:01 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -21,9 +21,10 @@ module.exports = function(eleventyConfig) {
         },
     });
 
-    // Pass through files
+    // Passthrough copies
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("admin");
 
     // Collections
